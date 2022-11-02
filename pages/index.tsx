3c0c81797f62053d83eafa4cc0ddfeb231776a93
Brian Temu <bryangasper2124@gmail.com>
2022-11-02 12:43:36 +0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,7 @@ export default function Home() {
     );
   }
 
-  function Cards(props: { name: string; icon: any; dec: string }) {
+  function Cards(props: { name: string; icon: JSX.Element; dec: string }) {
     return (
       <div className="flex flex-col  items-center justify-center space-y-4 bg-white bg-transparent  p-12 shadow-pink-300/20 hover:shadow-2xl dark:hover:shadow-pink-400">
         {props.icon}
@@ -91,11 +91,24 @@ export default function Home() {
     );
   }
 
+  function Card(props: { name: string; icon: JSX.Element; dec: string }) {
+    return (
+      <div className="flex flex-col  justify-center space-y-4 bg-white bg-transparent  p-12 shadow-2xl shadow-pink-300/30 dark:hover:shadow-pink-400">
+        {props.icon}
+
+        <h1 className="text-2xl dark:text-white">{props.name}</h1>
+        <h1 className="text-slate-500 dark:text-white">{props.dec}</h1>
+      </div>
+    );
+  }
+
   return (
     <div
       className={`${enabled ? "" : "dark"} relative h-auto overflow-x-hidden`}
     >
-      <main className=" bg-gradient-to-b from-[#fff] to-[#fff] dark:from-[#1A0B29] dark:to-[#07000A]">
+      <main className=" bg-gradient-to-b from-[#fff] to-[#fff] dark:from-[#1A0B29] dark:via-[#07000A]  dark:to-[#241731]">
+        <div></div>
+
         <div className=" z-100 fixed top-0 z-10 flex  w-full items-center justify-between bg-white/20 py-2 px-6 dark:bg-black/20 md:px-12">
           <Image src="/image/logo.png" alt="" width={90} height={60} />
           <ThemeSwitch />
@@ -333,6 +346,91 @@ export default function Home() {
                       stroke-linecap="round"
                       stroke-linejoin="round"
                       d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                    />
+                  </svg>
+                }
+              />
+            </div>
+          </div>
+
+          <div className="py-12">
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <h1 className="text-center text-3xl dark:text-white ">
+                What we do
+              </h1>
+              <h1 className="text-center md:w-3/6 md:text-lg text-slate-500 dark:text-white">
+                We are a group of content creators, branding professionals and
+                digital marketing specialists that provide branding, digital
+                marketing and content development services.
+              </h1>
+            </div>
+            <div className="mt-12 grid w-full grid-cols-1 gap-2 md:grid-cols-2 md:gap-8 xl:grid-cols-3 ">
+              <Card
+                name="Business Branding"
+                dec="We help buinesses create a strong and smart perception of its products or services in the customer's mind. From brand development strategy,brand design,brand style guide to rebranding already established businesses"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-12 w-12 dark:text-white"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"
+                    />
+                  </svg>
+                }
+              />
+              <Card
+                name="Content Development"
+                dec="We help businesses create digital content to be expressed through any selected digital channel. From blog posts, articles,email newsletters,social media content,print collateral,website copy,white papers to E-books"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-12 w-12 dark:text-white"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+                    />
+                  </svg>
+                }
+              />
+              <Card
+                name="Digital Marketing"
+                dec="We help businesses use selected digital channels to market products and services in order to reach consumers. From SEO, SEM, email marketing, social media marketing, SMS marketing to google business marketing"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-12 w-12 dark:text-white"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
                 }
